29b054490dcdf02be3a35ac27bb17b624961f85c
Dalibor Koenig <dalibor.koenig@gmail.com>
2021-08-11 09:07:36 +0200

    done

--- src/Components.js/Result.jsx
+++ src/Components.js/Result.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
 
 function Result(props) {
+  let history = useHistory();
   console.log(props);
   const percentage = () => {
     let perc = ((100 * props.score) / 12).toFixed(2)
@@ -9,7 +11,7 @@ function Result(props) {
       <div className="result">
         <h1>You answered {props.score} {props.score === 1 ? "question" : "questions"} correctly!</h1>
         <h1>That is {perc}%</h1>
-        <Button onClick={()=>window.location.reload()}>Go Again</Button>
+        <Button onClick={()=>history.push("/")}>Go Again</Button>
       </div>
     )
   }
